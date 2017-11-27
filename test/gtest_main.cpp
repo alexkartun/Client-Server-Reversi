9427234d977ecf48f0ab3f72c63cbca7f0d2ffa7
kartuna <alex.kartun@gmail.com>
2017-11-27 19:46:09 +0200

    sdf

--- test/gtest_main.cpp
+++ test/gtest_main.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-GTEST_API_ int main1(int argc, char **argv) {
+GTEST_API_ int main(int argc, char **argv) {
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
