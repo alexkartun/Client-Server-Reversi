e9ad460798897d77d4ad4db6ba2d30a541991d2b
kartuna <alex.kartun@gmail.com>
2017-12-27 21:54:06 +0200

    fd

--- Debug/src/client/src/Board.d
+++
@@ -1,4 +0,0 @@
-src/client/src/Board.d: ../src/client/src/Board.cpp \
- /home/kartuna/workspace/GitProject/src/client/include/Board.h
-
-/home/kartuna/workspace/GitProject/src/client/include/Board.h:
--- Debug/src/client/src/Client.d
+++
@@ -1,4 +0,0 @@
-src/client/src/Client.d: ../src/client/src/Client.cpp \
- /home/kartuna/workspace/GitProject/src/client/include/Client.h
-
-/home/kartuna/workspace/GitProject/src/client/include/Client.h:
